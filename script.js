1c51bdf45de0a7d6d841d20aaca77fa020fda572
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-09-15 15:23:45 -0400

    Test for special cases...

--- script.js
+++ script.js
@@ -51,6 +51,7 @@ function line(p1, p2) {
 }
 
 function mainFunction() {
+  //Get user input
   for(i=0; i<8; ++i) {
     if(isNaN(inputs[i].value)) {
       alert("Make sure inputs are numbers only!");
@@ -59,6 +60,11 @@ function mainFunction() {
   }
   line1 = new line(new point(l1p1x.value, l1p1y.value), new point(l1p2x.value, l1p2y.value));
   line2 = new line(new point(l2p1x.value, l2p1y.value), new point(l2p2x.value, l2p2y.value));
+
+  //Test for special cases
+  if(line1.m == line2.m) {
+    
+  }
 }
 
 runButton.addEventListener("click", mainFunction);
